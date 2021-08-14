e777b207b6d9108937f06d2a61dc320e174f2a1d
chouweihan <chouweihan@gmail.com>
2021-08-13 21:47:39 -0700

    adjustments

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const nodemailer = require("nodemailer");
 const validator = require("email-validator");
 const { StatusCodes } = require("http-status-codes");
 
-app.use(
-  cors({
-    origin: "https://www.chouweihan.com",
-    methods: ["GET", "POST"],
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors());
+
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.chouweihan.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post("/send-mail", async (req, res) => {
   let errCode;
