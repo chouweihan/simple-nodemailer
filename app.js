a73c0b23f1b83b4f4fdf346cbd992a48e3291f25
chouweihan <chouweihan@gmail.com>
2021-08-13 21:18:49 -0700

    e

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.chouweihan.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post("/send-mail", async (req, res) => {
   let errCode;
